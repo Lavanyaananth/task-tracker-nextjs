f95a887b85ca103c684198f237263294608990ae
lavanyaananth <lavanyaanand.11@gmail.com>
2026-06-18 13:55:24 +0530

    adding client component,studing use client directice, server component

---
+++ src/app/components/TaskCounter/TaskCounter.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+export default function TaskCounter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex items-center gap-4 p-4 border rounded-lg w-fit">
+      <p className="text-sm text-gray-500">Task completed</p>
+      <span className="font-bold text-2xl">{count}</span>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="px-3 py-1 bg-black text-white rounded text-sm"
+      >
+        + Done{" "}
+      </button>
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskCounter from "../components/TaskCounter/TaskCounter";
+function DashboardPage() {
+  return (
+    <div>
+      <p> Dashboard page</p>
+      <TaskCounter></TaskCounter>
+    </div>
+  );
+}
+
+export default DashboardPage;
